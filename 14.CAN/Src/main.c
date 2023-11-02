0c466bfa3df202ed362edcf727ed07fe585aa9f0
凯翔 闭 <1477530671@qq.com>
2023-11-02 21:21:41 +0800

    新4左右转4000

--- 14.CAN/Src/main.c
+++ 14.CAN/Src/main.c
@@ -51,7 +51,7 @@
 motor_measure_t motor_chassis[7];
 pid_struct_t pid_chassis[4];
 fp32 pid_parama[3] = {10,0,0};
-fp32 target_speed = 2000;
+fp32 target_speed = 4000;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
